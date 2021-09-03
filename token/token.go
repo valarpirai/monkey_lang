40861bbfdc02f70dca0c8432c084cc9c0aab077d
Valarpirairchandran A <a.valarpirai@gmail.com>
2021-09-03 20:55:16 +0530

    Basic lexer

---
+++ token/token.go
@@ -0,0 +1,38 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+	MULTI  = "*"
+	POWER  = "*"
+	DIVIDE = "/"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	// 1343456
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
