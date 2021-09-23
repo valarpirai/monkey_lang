4c1ab4edc4b6741901422a471a445da95f0b32ab
Valarpirairchandran A <a.valarpirai@gmail.com>
2021-09-23 20:05:16 +0530

    Parser init

---
+++ parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"monkey_lang/ast"
+	"monkey_lang/lexer"
+	"monkey_lang/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
